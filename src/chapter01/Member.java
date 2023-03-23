123dacfc59f49db6d3f8aefb8edd146544b48be1
dbrudthdus <dbrudthdus@gmail.com>
2023-03-23 17:00:53 +0900

    chapter 1~2

---
+++ src/chapter01/Member.java
@@ -0,0 +1,34 @@
+package chapter01;
+
+public class Member {
+
+	public static void main(String[] args) {
+		
+		// 이름 출력 메소드
+		System.out.println("심유경");
+		
+		
+		// 2-1
+		String name = "심유경";
+		int age = 29;
+		double height = 162.0;
+		boolean hasBook =true;
+		
+		
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(hasBook);
+		
+
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/chapter01/Person.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		String name;
+		String number;
+		String rrn;
+
+	}
+
+}
